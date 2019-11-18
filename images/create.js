22688c237913e073a23fc39070dba51f301add8c
GauravGarsa90 <gaurav.garsa@gmail.com>
2019-11-18 13:19:45 +0530

    update 1

---
+++ images/create.js
@@ -0,0 +1,28 @@
+import AWS from "aws-sdk";
+import * as dynamoDbLib from "./libs/dynamodb-lib";
+import { success, failure } from "./libs/response-lib";
+
+const dynamoDb = new AWS.DynamoDB.DocumentClient();
+
+export async function main(event, context, callback) {
+  // Request body is passed in as a JSON encoded string in 'event.body'
+  const data = JSON.parse(event.body);
+
+  const params = {
+    TableName: process.env.tableName,
+    Item: {
+      userId: event.requestContext.identity.cognitoIdentityId,
+      imageUrl: 'tmp',
+      name: data.name,
+      age: data.age,
+      createdAt: Date.now()
+    }
+  };
+
+  try {
+    await dynamoDbLib.call("put", params);
+    return success(params.Item);
+  } catch (e) {
+    return failure({ status: false });
+  }
+}
